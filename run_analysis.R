180aee03e6fe2769e66d9c5131698abcdab3256f
Lynx1981 <viktorija.leonova@gmail.com>
2015-11-21 19:41:06 +0200

    Create run_analysis.R
    This script processes the data from http://archive.ics.uci.edu/ml/datasets/Human+Activity+Recognition+Using+Smartphones, groups it by subject and activity and calculates the average from all means and standard deviation variables

---
+++ run_analysis.R
@@ -0,0 +1,27 @@
+test.data <- read.table("./UCI HAR Dataset/test/X_test.txt")
+test.subj <- read.table("./UCI HAR Dataset/test/subject_test.txt")
+test.activity <- read.table("./UCI HAR Dataset/test/y_test.txt")
+
+train.data <- read.table("./UCI HAR Dataset/train/X_train.txt")
+train.subj <- read.table("./UCI HAR Dataset/train/subject_train.txt")
+train.activity <- read.table("./UCI HAR Dataset/train/y_train.txt")
+
+all.data <-rbind(train.data, test.data)
+all.subj <- rbind(train.subj, test.subj)
+
+activity.names <- read.table("./UCI HAR Dataset/activity_labels.txt")
+all.activity <- rbind (train.activity, test.activity)
+all.activity.names <- merge (all.activity, activity.names)[,2]
+
+feature.names <- read.table("./UCI HAR Dataset/features.txt")
+names(all.data) <- feature.names[,2]
+
+selected.data<-all.data[,grepl("-mean()", colnames(all.data), fixed=TRUE)|grepl("-std()", colnames(all.data),fixed=TRUE)]
+dimensions <- cbind(all.subj, all.activity.names)
+names(dimensions) <- c("subject", "activity")
+
+tidy.data <- cbind(dimensions, selected.data)
+aggregated.data <- aggregate(tidy.data[,c(-1,-2)], list(tidy.data$subject, tidy.data$activity), FUN=mean, na.rm=TRUE)
+names(aggregated.data) <- sub("", "Average_", names(aggregated.data))
+names(aggregated.data)[1] <- "Subject"
+names(aggregated.data)[2] <- "Activity"
